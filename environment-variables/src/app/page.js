7432756519d1285062bb7207172afc7f46e8bdf5
subugee <sasubramanian_md@hotmail.com>
2025-08-18 12:30:30 +0530

    enviroment variables

---
+++ environment-variables/src/app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+
+  return <div className="m-50">
+    <h1>Environment Variables</h1>
+    <h1>.Env File : {process.env.MESSAGE}</h1>
+    <h2>.env.development : {process.env.MYVAR}</h2>
+    <h2>.env.production : {process.env.MYVARPROD}</h2>
+    <h2>.env.local : {process.env.MYVARLOCAL}</h2>
+    <h2>Greet Variable In All ENV : {process.env.GREET}</h2>
+
+  </div>
+}
