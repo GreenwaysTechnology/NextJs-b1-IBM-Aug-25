a79fe8d633f014a89913fd265bd610dbe9490ed1
subugee <sasubramanian_md@hotmail.com>
2025-08-21 13:22:01 +0530

    exception handling

---
+++ exception-app/src/app/not-found.jsx
@@ -0,0 +1,9 @@
+// app/not-found.jsx
+export default function NotFound() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">404 - User Not Found</h1>
+      <p>The user you are looking for doesn’t exist.</p>
+    </div>
+  );
+}
