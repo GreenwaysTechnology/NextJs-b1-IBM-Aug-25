9f675b385a9b03bdbb320455546ee0948c652aab
subugee <sasubramanian_md@hotmail.com>
2025-08-21 11:24:07 +0530

    seo

---
+++ seo-app/src/app/about/page.js
@@ -0,0 +1,10 @@
+
+//meta data for the about page
+export const metadata= {
+    title:'About',
+    description:'This is about page'
+}
+
+export default function About(){
+    return <h1>About</h1>
+}
---
+++ seo-app/src/app/todos/[id]/page.js
@@ -0,0 +1,50 @@
+export async function fetchTodosByid(id) {
+    const url = `https://jsonplaceholder.typicode.com/todos/${id}`
+    const response = await fetch(url)
+    return response.json()
+}
+
+export async function fetchTodos() {
+    const url = `https://jsonplaceholder.typicode.com/todos`
+    const response = await fetch(url)
+    return response.json()
+}
+
+//dynamic meta : it is supplied during request time or build time
+export async function generateMetadata({ params, searchParam }, parent) {
+    const id = (await params).id
+    const todo = await fetchTodosByid(Number(id))
+
+    //return meta object
+    return {
+        title: `${todo.title}`,
+        description: `${todo.title}`
+    }
+
+}
+
+export default async function TodoDetailPage(props) {
+    const id = (await props.params).id
+    const todo = await fetchTodosByid(+id)
+    return <div>
+        <h2>id {todo.id}</h2>
+        <p>title {todo.title}</p>
+        <p>Status {todo.completed ? 'Ok' : 'Not Completed'}</p>
+    </div>
+}
+
+//override funciton which is called during build time in order to prepare the page
+export async function generateStaticParams() {
+    const todos = await fetchTodos()
+    //generate html pages for all todos(200 todos), if you have 200 records, 200 html files
+    //to be generated
+
+    return todos.map(todo => {
+        //based on id only the pages are generated, id value must be string always.
+        let id = todo.id.toString()
+        return {
+            //id: id
+            id
+        }
+    });
+}
